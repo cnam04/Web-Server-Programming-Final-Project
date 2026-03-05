234c5c081a3d206282766daa3d605cbe3be6d5e5
cnam04 <colenam04@gmail.com>
2026-03-05 12:25:02 -0500

    Add page plan for midterm and create navbar

---
+++ client/plan/plan.md
@@ -0,0 +1,33 @@
+# Plan for client
+
+## Pages
+
+### Login Page -> *Landing page if not already logged in*
+I will do this when integrating server, since i can use SSO
+
+
+### Activity Feed ->*Landing page if logged in*
+Shows friends climbs and all climbs
+- you can filter by 
+
+### Add activity page 
+You should be able to add a session:
+- Each session has a type (indoor/outdoor)
+- Session should have a date/duration
+- other fields can be added later
+Each session should have multiple climbs:
+- Climbs should have optional name (name if outdoor, color if indoor)
+- grade
+- rating/10
+
+### Add friends
+Allow you to search for users
+- users pop up as a card with name,username,picture,location and plus sign
+- Clicking + sends friend request
+
+### Admin page
+Allows you to manage users
+- Edit user info
+- Delete user
+
+
---
+++ client/src/components/navbar.vue
@@ -0,0 +1,100 @@
+<template>
+
+
+<nav class="navbar" role="navigation" aria-label="main navigation">
+  <div id="navbarBasicExample" class="navbar-menu" :class="{ 'is-active': isMenuOpen }">
+    <div class="navbar-start">
+      <a class="navbar-item" @click="closeMenu">
+        Home
+      </a>
+
+      <a class="navbar-item" @click="closeMenu">
+        Documentation
+      </a>
+
+      <div class="navbar-item has-dropdown is-hoverable" :class="{ 'is-active': isMoreOpen }">
+        <a class="navbar-link" @click.prevent="toggleMore">
+          More
+        </a>
+
+        <div class="navbar-dropdown">
+          <a class="navbar-item" @click="closeAllMenus">
+            About
+          </a>
+          <a class="navbar-item is-selected" @click="closeAllMenus">
+            Jobs
+          </a>
+          <a class="navbar-item" @click="closeAllMenus">
+            Contact
+          </a>
+          <hr class="navbar-divider">
+          <a class="navbar-item" @click="closeAllMenus">
+            Report an issue
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+          <a class="button is-primary">
+            <strong>Sign up</strong>
+          </a>
+          <a class="button is-light" @click="closeMenu">
+            Log in
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
+
+
+</template>
+<script>
+export default {
+  name: 'Navbar',
+  data() {
+    return {
+      isMenuOpen: false,
+      isMoreOpen: false,
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.isMenuOpen = !this.isMenuOpen;
+      if (!this.isMenuOpen) {
+        this.isMoreOpen = false;
+      }
+    },
+    closeMenu() {
+      this.isMenuOpen = false;
+      this.isMoreOpen = false;
+    },
+    toggleMore() {
+      this.isMoreOpen = !this.isMoreOpen;
+    },
+    closeAllMenus() {
+      this.closeMenu();
+    },
+  },
+}
+</script>
+
+<style scoped>
+.brand-logo {
+  display: block;
+  width: clamp(120px, 26vw, 220px);
+  height: auto;
+}
+
+.navbar-brand {
+  min-width: 0;
+}
+.navbar-end {
+  margin-left: auto;
+}
+.navbar-start {
+  margin-right: auto;
+}
+</style>
